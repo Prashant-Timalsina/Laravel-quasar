d9f74531f0a9410899be5da344ef749d794d19f9
Prashant Timalsina <prashanttimalsina2@gmail.com>
2026-01-22 10:25:28 +0545

    Create Note form added

--- frontend/src/pages/CreateNote.vue
+++ frontend/src/pages/CreateNote.vue
@@ -1,3 +1,48 @@
 <template>
-  <div>Create</div>
+  <q-page class="row justify-center">
+    <q-card class="q-pa-md" style="min-width: 500px">
+      <q-card-section>
+        <div class="text-h6">Add Note</div>
+
+        <q-form @submit.prevent="submitForm" ref="formRef">
+          <q-input
+            v-model="form.title"
+            label="Title"
+            outlined
+            clearable
+            class="q-mb-md"
+            placeholder="Enter title"
+            required
+          />
+          <q-input
+            v-model="form.content"
+            label="Content"
+            outlined
+            clearable
+            class="q-mb-md"
+            placeholder="Enter content"
+            required
+          />
+          <q-btn type="submit" label="Submit" color="primary" />
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-page>
 </template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+
+const form = reactive({
+  title: '',
+  content: '',
+})
+
+const formRef = ref(null)
+function submitForm() {
+  console.log('Form Submitted:', form)
+  alert(`Note is: ${form.title} ${form.content}`)
+
+  formRef.value.reset()
+}
+</script>

3d1af50e62322239e52d8c2597a75e60d299ae02
Prashant Timalsina <prashanttimalsina2@gmail.com>
2026-01-22 10:40:31 +0545

    Placeholder User Profile made

--- frontend/src/pages/UserProfile.vue
+++ frontend/src/pages/UserProfile.vue
@@ -1,1 +1,21 @@
-<template><div>View Vue</div></template>
+<template>
+  <q-page class="row justify-center">
+    <div class="q-pt-xl">
+      <q-card class="q-pa-md text-center">
+        <q-avatar size="100px" color="grey-3" class="q-mb-md">
+          <q-icon name="account_circle" size="80px" color="grey-8" />
+        </q-avatar>
+
+        <h3 class="q-mb-xs">{{ placeholder.name }}</h3>
+        <p class="text-grey">{{ placeholder.role }}</p>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+const placeholder = {
+  name: 'Prashant',
+  role: 'Web Developer',
+}
+</script>
